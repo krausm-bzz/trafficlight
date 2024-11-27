d1d2041e5f65fe62708478b35ae23b8b078f8087
krausm-bzz <krausm@bzz.ch>
2024-11-27 13:56:51 +0100

    first commit for M324

---
+++ trafficlight.test.js
@@ -0,0 +1,19 @@
+const app = require("./trafficlight");
+
+describe('test suite traffic light', () => {
+    it(`case red light`, () => {
+        expect(app.trafficLight("red")).toBe("DON'T WALK!");
+    });
+
+    it(`case orange light`, () => {
+        expect(app.trafficLight("orange")).toBe("ATTENTION!");
+    });
+
+    it(`case green light`, () => {
+        expect(app.trafficLight("green")).toBe("WALK!");
+    });
+
+    it(`case no argument (out of order)`, () => {
+        expect(app.trafficLight()).toBe("OUT OF ORDER");
+    });
+});
